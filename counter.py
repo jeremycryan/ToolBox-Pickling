652c9d937bf9b8c745be08556280b24be9d4e4cd
Jeremy <jeremychristopherryan@gmail.com>
2017-03-13 23:26:34 -0400

    Finished pickling.

--- counter.py
+++ counter.py
@@ -3,38 +3,23 @@
 from os.path import exists
 import sys
 from pickle import dump, load
+import codecs
 
 
 def update_counter(file_name, reset=False):
-    """ Updates a counter stored in the file 'file_name'
-
-    A new counter will be created and initialized to 1 if none exists or if
-    the reset flag is True.
-
-    If the counter already exists and reset is False, the counter's value will
-    be incremented.
-
-    file_name: the file that stores the counter to be incremented.  If the file
-    doesn't exist, a counter is created and initialized to 1.
-    reset: True if the counter in the file should be rest.
-    returns: the new counter value
-
-    >>> update_counter('blah.txt',True)
-    1
-    >>> update_counter('blah.txt')
-    2
-    >>> update_counter('blah2.txt',True)
-    1
-    >>> update_counter('blah.txt')
-    3
-    >>> update_counter('blah2.txt')
-    2
-    """
-    pass
+    if not reset and exists(file_name):
+        file1 = open(file_name, 'rb')
+        counter = load(file1) + 1
+        file1.close()
+        file2 = open(file_name, 'wb')
+        dump(counter, file2)
+        file2.close()
+        return counter
+    else:
+        file1 = open(file_name, 'wb')
+        dump(1, file1)
+        file1.close()
+        return 1
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        import doctest
-        doctest.testmod()
-    else:
-        print("new value is " + str(update_counter(sys.argv[1])))
+    print("new value is " + str(update_counter('blah.txt')))
